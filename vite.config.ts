2582298119518d997ac7368fbc87380e98ef0459
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-04-09 23:56:00 -0700

    trying all sorts of methods for theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        paths: ["./node_modules/semantic-ui-less"],
+      },
+    },
+  },
+});
